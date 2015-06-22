c44467eade30b539b65b7c7ca74b5ddc0aa207a5
radjiv.js <carrereradjiv@yahoo.fr>
2015-06-22 14:50:42 +0200

    Update addMailAuthors.js

--- addMailAuthors.js
+++ addMailAuthors.js
@@ -11,12 +11,11 @@ slides = fs.readdirSync(slidesDir).filter(junk.not);
 console.log(slides)
 for(slide in slides){
 	var slidePath = slidesDir+slides[slide];
-	//var file = jf.readFileSync(slidePath);
 	var slideData = JSON.parse(fs.readFileSync(slidePath));
  	var authors=[];
 	if(slideData.json.authors){
-		// authors.push("team@coorpacademy.com");
-		// fs.writeFileSync(slidePath, format(slideData));
+		 authors.push("team@coorpacademy.com");
+		 fs.writeFileSync(slidePath, format(slideData));
 	}	
 	else{	
 		slideData.json.authors = ["team@me.com"];
